72415e0ecac2195be08c11d841f736bf4e8ba1ab
Moe-Baker <moe4baker@gmail.com>
2019-09-23 12:42:46 +0200

    Work

---
+++ Assets/Objects/Entity/Utility/EntityStatusEffectParticles.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.AI;
+
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditorInternal;
+#endif
+
+using Object = UnityEngine.Object;
+using Random = UnityEngine.Random;
+
+namespace Game
+{
+	public class EntityStatusEffectParticles : Entity.Module
+	{
+		[SerializeField]
+        protected ParticleSystem system;
+        public ParticleSystem System { get { return system; } }
+
+        public bool Emission
+        {
+            set
+            {
+                var module = system.emission;
+
+                module.enabled = value;
+            }
+        }
+
+        [SerializeField]
+        protected StatusEffectType type;
+        public StatusEffectType Type { get { return type; } }
+
+        protected virtual void Reset()
+        {
+            system = Dependancy.Get<ParticleSystem>(gameObject);
+        }
+
+        public override void Init()
+        {
+            base.Init();
+
+            UpdateState();
+
+            Entity.StatusEffects.OnAdd += AddCallback;
+            Entity.StatusEffects.OnRemove += RemoveCallback;
+        }
+
+        void UpdateState()
+        {
+            Emission = Entity.StatusEffects.Contains(type);
+        }
+
+        void AddCallback(StatusEffectInstance effect)
+        {
+            UpdateState();
+        }
+
+        void RemoveCallback(StatusEffectType type)
+        {
+            UpdateState();
+        }
+    }
+}
---
+++ Assets/Objects/Status Effect/Type/Fire/FireStatusEffect.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.AI;
+
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditorInternal;
+#endif
+
+using Object = UnityEngine.Object;
+using Random = UnityEngine.Random;
+
+namespace Game
+{
+    [CreateAssetMenu(menuName = MenuPath + "Fire")]
+	public class FireStatusEffect : StatusEffectType
+	{
+        public override void Apply(StatusEffectInstance effect)
+        {
+            effect.Affector.DoDamage(effect.Data.Potency, Damage.Method.Ranged, effect.Target);
+        }
+    }
+}
