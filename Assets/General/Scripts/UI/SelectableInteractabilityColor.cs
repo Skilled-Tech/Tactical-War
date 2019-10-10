800cb2acaa39289816724558ddda73803304c83a
Moe-Baker <moe4baker@gmail.com>
2019-10-10 10:21:48 +0200

    Work

---
+++ Assets/General/Scripts/UI/SelectableInteractabilityColor.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.AI;
+
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditorInternal;
+#endif
+
+using Object = UnityEngine.Object;
+using Random = UnityEngine.Random;
+
+namespace Game
+{
+    [RequireComponent(typeof(Graphic))]
+	public class SelectableInteractabilityColor : MonoBehaviour
+	{
+		[SerializeField]
+        protected Selectable source;
+        public Selectable Source { get { return source; } }
+
+        [SerializeField]
+        protected Color on;
+        public Color On { get { return on; } }
+
+        [SerializeField]
+        protected Color off;
+        public Color Off { get { return off; } }
+
+        public Graphic Graphic { get; protected set; }
+
+        private bool lastInteractableValue;
+
+        protected virtual void Reset()
+        {
+            source = Dependancy.Get<Selectable>(gameObject, Dependancy.Scope.RecursiveToParents);
+        }
+
+        protected virtual void Awake()
+        {
+            Graphic = GetComponent<Graphic>();
+        }
+
+        protected virtual void Start()
+        {
+            UpdateState();
+
+            lastInteractableValue = source.interactable;
+        }
+
+        protected virtual void Update()
+        {
+            if (lastInteractableValue != source.interactable)
+            {
+                UpdateState();
+                lastInteractableValue = source.interactable;
+            }
+        }
+
+        protected virtual void UpdateState()
+        {
+            Graphic.color = source.interactable ? on : off;
+        }
+    }
+}
