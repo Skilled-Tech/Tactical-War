2ffa253283fc0e467118528336ef8492713353b5
Moe-Baker <moe4baker@gmail.com>
2020-01-05 13:13:02 +0200

    Work

---
+++ Assets/Editor/AutoSignAndroidKeyStore.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.AI;
+
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditorInternal;
+#endif
+
+using Object = UnityEngine.Object;
+using Random = UnityEngine.Random;
+
+namespace Game
+{
+	public static class AutoSignAndroidKeyStore
+	{
+        [InitializeOnLoadMethod]
+        static void OnLoad() => Execute();
+
+        [MenuItem("Tools/Enter Android Keys")]
+        static void MenuItem() => Execute();
+
+        public static void Execute()
+        {
+            PlayerSettings.Android.keystorePass = "^5bBx>MJ75";
+            PlayerSettings.Android.keyaliasPass = "^5bBx>MJ75";
+        }
+	}
+}
